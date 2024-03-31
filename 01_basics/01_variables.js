252ad7ef5733c3083be5330c8e90035304546924
WAJID ALI HASHMI <113199214+Wajidalihashmi29@users.noreply.github.com>
2024-03-31 10:31:19 +0000

    Modified and added new files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 96923
+let accountEmail = "wajid@google.com"
+var accountPass = "12345"
+/*
+var should not be used because of block and functional scope issues
+*/
+accountCity = "Bhopal"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all code as newer version
+
+//alert(3+3)//we are using nodejs, not browser
+let name = "wajid"
+let age = 18
+let isLoggedIn = false 
+//number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => no value assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof"wajid");
