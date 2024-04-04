a26f681722b34096f2984316c37e8608b70e7733
WAJID ALI HASHMI <113199214+Wajidalihashmi29@users.noreply.github.com>
2024-04-04 03:08:34 +0000

    Created and Modified

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "33"
+/*let score = "33"
 console.log(score);
 console.log(typeof(score));
 
 let value = Number(score)
 console.log(typeof value);
 console.log(value);
-
+*/
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
@@ -13,6 +13,42 @@ console.log(value);
 
 let isLoggedIn = null
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1/string => true; null/empty/0 => false 
+// **********Operations********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " wajid"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
